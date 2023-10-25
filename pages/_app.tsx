3dd5c36c0d7829c1114ba45df4ac13d1e078b21e
nshcoding <ice44xx@gmail.com>
2023-10-25 17:19:41 -0300

    implemantação google OAuth login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 config.autoAddCss = false;
 
 import DefaultLayout from "@/layouts/default-layout";

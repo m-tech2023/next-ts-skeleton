832920aa5dfafa7cd6e7567e3723eaf30c8ddbb6
pedro <pedro.admoura@gmail.com>
2023-10-27 18:31:25 -0300

    feat: add google provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,17 @@ import DefaultLayout from '@/layouts/default-layout';
 import store from '@/store';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <GoogleOAuthProvider clientId="1091851264293-rjetgf1fetcv18aes1d9g2cu7ir1am53.apps.googleusercontent.com">
+
     <Provider store={store}>
       <DefaultLayout>
         <Component {...pageProps} />
       </DefaultLayout>
     </Provider>
+    </GoogleOAuthProvider>
   );
 }

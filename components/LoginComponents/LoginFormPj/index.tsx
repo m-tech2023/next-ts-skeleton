dfd56f8b4b90e217598cad96eea48c87b33e157d
nshcoding <ice44xx@gmail.com>
2023-10-27 14:14:57 -0300

    removido login google de pessoa jurídica

--- components/LoginComponents/LoginFormPj/index.tsx
+++ components/LoginComponents/LoginFormPj/index.tsx
@@ -4,35 +4,8 @@ import LoginLabel from "../label";
 import LoginInput from "../input";
 import Link from "next/link";
 import LoginButton from "../button";
-import { useGoogleLogin } from "@react-oauth/google";
-import axios from "axios";
 
 const LoginFormPessoaJuridica = () => {
-  const loginAcess = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      try {
-        const userInfo = await axios.get(
-          "https://www.googleapis.com/oauth2/v3/userinfo",
-          {
-            headers: {
-              Authorization: `Bearer ${tokenResponse.access_token}`,
-            },
-          }
-        );
-        if (userInfo) {
-          const userData = userInfo.data;
-          console.log(userData);
-        } else {
-          console.log("Resposta vazia ou sem dados de usuário.");
-        }
-      } catch (error) {
-        console.error("Erro ao obter informações do usuário:", error);
-      }
-    },
-    onError: (error) => {
-      console.error("Erro ao fazer login com o Google:", error);
-    },
-  });
   return (
     <form className={styles.form}>
       <div className={styles.form_group}>
@@ -68,12 +41,6 @@ const LoginFormPessoaJuridica = () => {
       </div>
       <div className={styles.container_buttons}>
         <LoginButton text="ENTRAR" className={styles.btn} type="submit" />
-        <LoginButton
-          onClick={() => loginAcess()}
-          img="/google-icon.png"
-          text="ENTRAR COM GOOGLE"
-          className={styles.btn_google}
-        />
       </div>
     </form>
   );

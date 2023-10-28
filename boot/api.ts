cdb2578ef04386f1590bcd55cc2d91de0e5008bb
Cristiano <cristiano.justino@m-tech.digital>
2023-10-28 20:54:32 -0300

    fix simples

--- boot/api.ts
+++ boot/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const api = axios.create({
   baseURL: process.env.API_URL,
   headers: {
@@ -10,11 +9,3 @@ const api = axios.create({
 });
 
 export default api;
-=======
-const createApi = (endpoint: string) => {
-  const baseURL = `https://pokeapi.co/api/v2/${endpoint}`;
-  return axios.create({ baseURL });
-};
-
-export default createApi;
->>>>>>> implementado sistema de busca via router query, layout terminado, esperando back-end

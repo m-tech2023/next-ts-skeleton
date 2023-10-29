3dd89859768bc4dd27d8a5b72a56dfbaa331934a
nshcoding <ice44xx@gmail.com>
2023-10-29 15:33:25 -0300

    implementado correção no layout dos componentes common

--- components/Common/BreadCrumb/index.tsx
+++ components/Common/BreadCrumb/index.tsx
@@ -2,23 +2,21 @@ import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 interface BreadCrumbProps {
   lastText: string;
 }
 
-const BreadCrumb: React.FC<BreadCrumbProps> = ({
-  lastText,
-}) => {
+const BreadCrumb: React.FC<BreadCrumbProps> = ({ lastText }) => {
   const router = useRouter();
   const pathnames = router.asPath.split("/").filter((x) => x);
 
   return (
     <div className={styles.container_breadcrumb}>
-      <Link href="/">
+      <Link href="/" className={styles.link}>
         Home
-        <FontAwesomeIcon icon={faAngleRight} />
+        <FontAwesomeIcon icon={faAngleRight} className={styles.icon} />
       </Link>
       {pathnames.map((pathname, index) => {
         const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;

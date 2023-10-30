14b2d3da46d5150bc4b14e26d6ebfd0abf5f347c
nshcoding <ice44xx@gmail.com>
2023-10-30 15:40:11 -0300

    feat: inserido login em EST, PJ

--- components/LoginComponents/LoginFormEstr/index.tsx
+++ components/LoginComponents/LoginFormEstr/index.tsx
@@ -1,13 +1,49 @@
 import styles from "../../../pages/login/styles.module.scss";
-import React from "react";
+import React, { FormEvent } from "react";
 import LoginLabel from "../Label";
 import LoginInput from "../Input";
 import Link from "next/link";
 import LoginButton from "../Button";
+import authService from "@/services/auth/auth.service";
+import { useRouter } from "next/router";
 
 const LoginFormPessoaEstrangeira = () => {
+  const router = useRouter();
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get("username")!.toString();
+    const password = formData.get("password")!.toString();
+
+    const attributes = { username, password };
+
+    try {
+      const { status } = await authService.login(attributes);
+      if (status === 200 || status === 201) {
+        console.log("logado");
+        router.push("/area-cliente/dados-pessoais");
+      } else {
+        console.log("Algo deu errado no servidor. Tente novamente mais tarde.");
+      }
+    } catch (error) {
+      console.error("Erro ao efetuar o login:", error);
+
+      if (error.response) {
+        console.log(`Status de erro: ${error.response.status}`);
+        console.log(`Mensagem de erro: ${error.response.data.message}`);
+      } else if (error.message) {
+        console.log(`Erro: ${error.message}`);
+      } else {
+        console.log(
+          "Erro desconhecido. Verifique sua conexão e tente novamente."
+        );
+      }
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleLogin}>
       <div className={styles.form_group}>
         <LoginLabel
           labelFor="email"

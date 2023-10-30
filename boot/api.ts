8cc7732ce939a74df4ec47ed7011eb71f3b06112
nshcoding <ice44xx@gmail.com>
2023-10-30 20:17:15 -0300

    fix: boot/api

--- boot/api.ts
+++ boot/api.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://localhost:3000/',
-=======
-  baseURL: "http://localhost:3000",
->>>>>>> feat: inserido auth service e handleLogin em PF
+  baseURL: "http://localhost:3000/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",

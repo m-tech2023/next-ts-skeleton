60cdec7c4dfd47f1e9a930eb4a4a42d568322e86
nshcoding <ice44xx@gmail.com>
2023-10-30 12:34:20 -0300

    feat: inserido auth service e handleLogin em PF

--- boot/api.ts
+++ boot/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: 'http://localhost:3000/',
+=======
+  baseURL: "http://localhost:3000",
+>>>>>>> feat: inserido auth service e handleLogin em PF
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",

c5aa972e32f83f1c3caec3d7ca9dfd5f43e7ab95
nshcoding <ice44xx@gmail.com>
2023-10-30 20:52:56 -0300

    fix: rebase da dev

--- pages/login/forget-password/index.tsx
+++ pages/login/forget-password/index.tsx
@@ -10,13 +10,11 @@ const ForgetPassword = () => {
   return (
     <div className={styles.container}>
       <BreadCrumb lastText={styles.lastName} />
-      <div className={styles.container_title}>
-        <Header
-          title="Esqueci minha senha"
-          subTitle=" Digite o seu e-mail de cadastro para receber um link de recuperação de
+      <Header
+        title="Esqueci minha senha"
+        subTitle=" Digite o seu e-mail de cadastro para receber um link de recuperação de
           senha, onde será possível alterar a sua senha."
-        />
-      </div>
+      />
       <div className={styles.container_form}>
         <form className={styles.form}>
           <div className={styles.form_group}>

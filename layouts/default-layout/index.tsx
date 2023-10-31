7086a1cb2517b85cac14875eb9b765153990c25f
nshcoding <ice44xx@gmail.com>
2023-10-31 12:57:33 -0300

    feat: inserido whatsapp e instagram no layout default

--- layouts/default-layout/index.tsx
+++ layouts/default-layout/index.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Common/Footer";
 import Navbar from "@/components/Common/Navbar";
+import Whatsapp from "@/components/Common/Whatsapp";
 
 const DefaultLayout = ({ children }) => {
   return (
     <div>
       <Navbar />
-        {children}
+      <Whatsapp />
+      {children}
       <Footer />
     </div>
   );

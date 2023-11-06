5273081b3fa6ef425daf89c370f53e3ddae8645a
nshcoding <ice44xx@gmail.com>
2023-11-06 09:42:47 -0300

    fix: layout carousel

--- pages/example/index.tsx
+++ pages/example/index.tsx
@@ -13,7 +13,7 @@ const slides = [
     end: "31/10/2023 às 18:00",
     races: ["Appaloosa", "Brasileiro de Hipismo"],
     image:
-      "https://files.axshare.com/gsc/VPA5BX/58/62/f8/5862f8046670489a9b64a09628bcd39b/images/home/u1.png?pageId=410ff80e-364a-4d6b-ba72-a7540e2c7725",
+      "https://i.pinimg.com/originals/f0/37/e6/f037e664e26be6cbae951ff8e7091424.jpg",
     alt_image: "Cavalo",
   },
   {
@@ -24,7 +24,7 @@ const slides = [
     end: "03/11/2023 às 20:00",
     races: ["Paint Horse", "Quarter Horse"],
     image:
-      "https://www.petz.com.br/blog/wp-content/uploads/2023/07/racas-de-cavalo3.jpg",
+      "https://4.bp.blogspot.com/-eH76bdsRZVM/UwAvxIn1ttI/AAAAAAAAAC0/3DRX_9TCBqc/s1600/sera-que-ainda-existem-cavalos-selvagens-+(1).jpg",
     alt_image: "Cavalo",
   },
   {
@@ -35,7 +35,7 @@ const slides = [
     end: "03/11/2023 às 20:00",
     races: ["Paint Horse", "Quarter Horse"],
     image:
-      "https://www.lancerural.com.br/wp-content/uploads/2022/04/CRIOULO.jpg",
+      "https://s1.1zoom.me/b4455/591/Horses_Sand_Run_Brown_561328_1920x1080.jpg",
     alt_image: "Cavalo",
   },
 ];
